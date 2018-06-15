2a6ff95f04dbe46b46521b4c8dd5fe9eb8dd326b
Tayt Low <nictayt@gmail.com>
2018-06-14 23:06:23 -0600

    updating portfolio section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -22,7 +22,8 @@ const Contact = (props) => (
                     fontSize: '15px',
                     marginTop: '1em'
                     }}>
-                    <span style={{textDecoration: 'underline'}}>Only / Once (E-Commerce Clone)</span>
+                    <span style={{textDecoration: 'underline'}}>Only / Once (E-Commerce Clone)</span><br/>
+                    <span><a href="https://github.com/dank-lemonboi/only-once-clone"></a>https://github.com/dank-lemonboi/only-once-clone</span>
                     </div>
                 <a href="http://clone-once.com/#/">
                     <img style={{
@@ -30,18 +31,22 @@ const Contact = (props) => (
                         marginLeft: '30%'
                     }} src={personal} alt="Personal Project (E-Commerce)"/></a>
                 </div>
+                <div>
                    <a href="https://food-connect.net/#/"> <img style={{
-                        marginLeft: '3em',
-                        height: '14em'
+                        height: '14em',
+                        marginLeft: '-18em'
                     }}
                     src={group} alt="Group Project"/></a>
-                   <span style={{ 
+                   <div style={{ 
                        color: '#1F1300',
-                       position: 'absolute',
-                       left: '25em',
-                       textDecoration: 'underline',
-                       marginTop: '1em'
-                }}>Food-Connect</span>
+                       marginLeft: '-2em',
+                }}><span 
+                style={{
+                    textDecoration: 'underline'
+                    }}>Food-Connect</span><br/>
+                    <span>https://github.com/ScottWorks/Food-Connect</span><br/>
+                </div>
+                </div>
     </section>
 )
 
