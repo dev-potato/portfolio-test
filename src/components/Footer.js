9298441d3b624d9929c0c91fe7d1e96dd9913af1
Tayt Low <nictayt@gmail.com>
2018-06-25 15:31:50 -0600

    fixed github link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ const Footer = (props) => (
             <ul className="icons">
                 <li><span style={{fontSize: '3em', color: '#1F1300', marginRight: '9em'}}>Let's Connect.</span></li>
                 <li><a style={{fontSize: '2em', color: '#1F1300'}} href="https://twitter.com/devpotato1" className="icon alt fa-twitter"><span className="label">Twitter</span></a></li>
-                <li><a style={{fontSize: '2em', color: '#1F1300'}} href="https://github.com/dank-lemonboi" className="icon alt fa-github"><span className="label">GitHub</span></a></li>
+                <li><a style={{fontSize: '2em', color: '#1F1300'}} href="https://github.com/dev-potato" className="icon alt fa-github"><span className="label">GitHub</span></a></li>
                 <li><a style={{fontSize: '2em', color: '#1F1300'}} href="https://www.linkedin.com/in/taytlow/" className="icon alt fa-linkedin"><span className="label">LinkedIn</span></a></li>
             </ul>
             <div style={{
