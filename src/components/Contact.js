1691bc494548da46023764a280bb33ee33161a36
Tayt Low <nictayt@gmail.com>
2018-07-01 13:31:32 -0600

    making contact section mobile responsive

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,111 +2,78 @@ import React from 'react'
 import personal from '../assets/images/personal_project_picture.jpeg'
 import group from '../assets/images/group_project.png'
 
-const Contact = () => (
-    <section id="contact">
-        <span style={{
-            color: '#1F1300',
-            fontSize: '36px',
-            margin: '3em',
-            marginLeft: '45%',
-        }}>My Work.</span>
+import '../assets/scss/projectStyle.scss'
 
-         <div style={{
-            width: '100%',
-            display: 'flex',
-            boxSizing: 'border-box',
-            overflow: 'none'
-        }}>
-          <div style={{
-                color: '#1F1300',
-                width: '70%',
-                padding: '0',
-                marginLeft: '-24em'
-                // marginLeft: '-10em',
-                // marginTop: '3em',
-                // paddingRight: '10px',
-            }}>
 
-                <span style={{ 
-                    textDecoration: 'underline',
-                    fontSize: '24px',
-                    color: '#1F1300' }}>Only<span style={{color: 'red'}}>/</span>Once (E-Commerce Clone)</span><br />
+const width = window.innerWidth
+class Contact extends React.Component {
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            mobile: false
+        }
+    }
+    
+    componentWillMount() {
+        console.log({width})
+        
+        if(width < 600){
+            this.setState({
+                mobile: !this.state.mobile
+            })
+        }
+        console.log(this.state.mobile)
+    }
+    render() {
+        return(
+        <section id="contact">
+            <span className='work-section-title'>My Work.</span>
+    
+             <div className='project-card-wrapper'>
+              <div className='project-card-description'>
+    
+                    <span className='project-card-title'>Only<span style={{color: 'red'}}>/</span>Once (E-Commerce Clone)</span><br />
+    
+                    <span>This was my first project. My main focus here was to re-create a fully functioning 
+                    E-Commerce website with a pleasant user experience. I wanted to create the UX features 
+                    on my own using only React and CSS3 on my front end, built with a Node.js and 
+                    postgreSQL backend.</span><br/>
+                    <span> You can visit the site <a href="http://clone-once.com/#/"><span>HERE</span></a><br/>
+                        Or checkout the GitHub repo <a href="https://github.com/dank-lemonboi/only-once-clone"><span>HERE</span></a>
+                        </span>
+                </div>
+                    <div className='image-wrapper'>
+                        <img src={personal} alt="Personal Project (E-Commerce)" />
+                    </div>
+    
+                </div>
 
-                <span style={{
-                    width: '600px',
-                    color: '#1F1300'
-                }}>This was my first project. My main focus here was to re-create a fully functioning 
-                E-Commerce website with a pleasant user experience. I wanted to create the UX features 
-                on my own using only React and CSS3 on my front end, built with a Node.js and 
-                postgreSQL backend.</span><br/>
-                <span> You can visit the site <a href="http://clone-once.com/#/"><span>HERE</span></a><br/>
-                    Or checkout the GitHub repo <a href="https://github.com/dank-lemonboi/only-once-clone"><span>HERE</span></a>
-                    </span>
+            <div className='project-card-wrapper'>
+            <div className='project-card-description'>
+                <span className='project-card-title'>Food-Connect</span><br />
+                    
+                    <span>The objective of this Application is to help reduce food waste.
+                        Our goal is to provide a simple and effective platform for small
+                        business-owners to easily give their would be waste to pantries
+                        and soup kitchens. Our aim is to provide Tax Documents so the
+                        businesses can receive a charitable donation tax deduction,
+                        and we provide analytics so the people using the App can see how
+                        much food they have saved from the landfill and their accumulated
+                        tax savings.</span><br/>
+                        <span> You can visit the site <a href="https://food-connect.net/#/"><span>HERE</span></a><br/>
+                        Or checkout the GitHub repo <a href="https://github.com/ScottWorks/Food-Connect"><span>HERE</span></a>
+                        </span>
+                </div>
+                <div className='image-wrapper'>
+                 <img src={group} alt="Group Project" />
+             </div>
             </div>
+        </section>
 
-            <div
-                style={{
-                    width: '50%',
-                    marginLeft: '-13.5em'
-                }}
-            >
-                <img style={{
-                    height: '16em',
-                   paddingBottom: '2em'
-                }} src={personal} alt="Personal Project (E-Commerce)" />
-            </div>
-            </div>
-        <div style={{
-            width: '100%',
-            display: 'flex',
-            boxSizing: 'border-box',
-            overflow: 'none'
-        }}>
-        <div style={{
-                color: '#1F1300',
-                width: '70%',
-                padding: '0',
-                marginLeft: '-24em'
-                // marginLeft: '-10em',
-                // marginTop: '3em',
-                // paddingRight: '10px',
-            }}>
-            <span
-                style={{
-                    textDecoration: 'underline',
-                    fontSize: '24px',
-                    color: '#1F1300'
-                }}>Food-Connect</span><br />
-                
-                <span style={{
-                    width: '600px',
-                    color: '#1F1300'
-                }}>The objective of this Application is to help reduce food waste.
-                    Our goal is to provide a simple and effective platform for small
-                    business-owners to easily give their would be waste to pantries
-                    and soup kitchens. Our aim is to provide Tax Documents so the
-                    businesses can receive a charitable donation tax deduction,
-                    and we provide analytics so the people using the App can see how
-                    much food they have saved from the landfill and their accumulated
-                    tax savings.</span><br/>
-                    <span> You can visit the site <a href="https://food-connect.net/#/"><span>HERE</span></a><br/>
-                    Or checkout the GitHub repo <a href="https://github.com/ScottWorks/Food-Connect"><span>HERE</span></a>
-                    </span>
-            </div>
-            <div
-                style={{
-                    width: '50%',
-                    marginLeft: '-16em'
-                }}
-            >
-             <img style={{
-                height: '20em',
-                paddingBottom: '2em'
-            }}
-                src={group} alt="Group Project" />
-         </div>
-        </div>
-    </section>
-)
+        )
+    }
+} 
+
 
 export default Contact
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,22 +4,19 @@ import React from 'react'
 const Footer = (props) => (
     <footer id="footer">
         <div className="inner">
-            <ul className="icons">
-                <li><span style={{fontSize: '3em', color: '#1F1300', marginRight: '9em'}}>Let's Connect.</span></li>
-                <li><a style={{fontSize: '2em', color: '#1F1300'}} href="https://twitter.com/devpotato1" className="icon alt fa-twitter"><span className="label">Twitter</span></a></li>
-                <li><a style={{fontSize: '2em', color: '#1F1300'}} href="https://github.com/dev-potato" className="icon alt fa-github"><span className="label">GitHub</span></a></li>
-                <li><a style={{fontSize: '2em', color: '#1F1300'}} href="https://www.linkedin.com/in/taytlow/" className="icon alt fa-linkedin"><span className="label">LinkedIn</span></a></li>
-            </ul>
-            <div style={{
-                width: '40%',
-                margin: '0 auto'
-            }}>
+        <span>Let's Connect.</span>
+            <div className='email-form'>
                 <form method="POST" action="https://formspree.io/nictayt.dev@gmail.com">
                     <input style={{border:'1px solid #1F1300', color: '1px solid #1F1300'}} type="email" name="email" placeholder="Your email"/>
                     <textarea style={{border:'1px solid #1F1300', color: '1px solid #1F1300', marginTop: '5px'}} name="message" placeholder="Lets work together."></textarea>
                     <button style={{border:'1px solid #1F1300', color: '1px solid #1F1300', marginTop: '1em', color: '#1F1300'}} type="submit">Send</button>
                 </form>
             </div>
+            <ul className="icons">
+                <li><a href="https://twitter.com/devpotato1" className="icon alt fa-twitter"><span className="label">Twitter</span></a></li>
+                <li><a href="https://github.com/dev-potato" className="icon alt fa-github"><span className="label">GitHub</span></a></li>
+                <li><a href="https://www.linkedin.com/in/taytlow/" className="icon alt fa-linkedin"><span className="label">LinkedIn</span></a></li>
+            </ul>
             <ul className="copyright">
                 <li>&copy; dev-potato</li><li>Gatsby.js</li>
             </ul>
