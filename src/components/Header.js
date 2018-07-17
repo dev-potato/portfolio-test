4ba2590463f57ccdbba446754a16b8e48efb655f
Tayt Low <nictayt@gmail.com>
2018-07-17 17:10:16 -0600

    created dynamic variables so the navbar works regardless of screen size

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,16 +2,23 @@ import React from 'react'
 import Link from 'gatsby-link'
 import Logo from '../assets/images/NTL-badge.png'
 
+const pageHeight = window.innerHeight;
+
+let me = pageHeight * 0.93;
+let tech = pageHeight * 1.58;
+let port = pageHeight * 3.099;
+let con = pageHeight * 5;
+
 const Header = (props) => (
     
     <header id={props.header} className="alt">
         <Link to="/" className="logo"><span style={{marginLeft: '.75em', fontSize: '9em', letterSpacing: '4px', fontWeight: '400'}}>Low.</span></Link>
         <nav className='site-nav'>
             <ul style={{listStyle: 'none', display: 'flex'}}>
-                <li onClick={ () => window.scrollTo(0, 650)} style={{marginRight: '10px', fontWeight: '300px', color: '#EEE5E9'}}>Me.</li>
-                <li onClick={ () => window.scrollTo(0, 1230)} style={{marginRight: '10px', fontWeight: '300px', color: '#EEE5E9'}}>Technology.</li>
-                <li onClick={ () => window.scrollTo(0, 2045)} style={{marginRight: '10px', fontWeight: '300px', color: '#EEE5E9'}}>Portfolio.</li>
-                <li onClick={ () => window.scrollTo(0, 3000)} style={{marginRight: '10px', fontWeight: '300px', color: '#EEE5E9'}}>Connect.</li>                                                
+                <li onClick={ () => window.scrollTo(0, me)} style={{marginRight: '10px', fontWeight: '300px', color: '#EEE5E9'}}>Me.</li>
+                <li onClick={ () => window.scrollTo(0, tech)} style={{marginRight: '10px', fontWeight: '300px', color: '#EEE5E9'}}>Technology.</li>
+                <li onClick={ () => window.scrollTo(0, port)} style={{marginRight: '10px', fontWeight: '300px', color: '#EEE5E9'}}>Portfolio.</li>
+                <li onClick={ () => window.scrollTo(0, con)} style={{marginRight: '10px', fontWeight: '300px', color: '#EEE5E9'}}>Connect.</li>                                                
             </ul>
         </nav>
     </header>
