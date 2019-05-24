46f343a8a0839ffa370f9b3b83479216050f29fb
Tayt Low <nictayt@gmail.com>
2019-05-23 18:53:49 -0600

    updating minor styles and portfolio data for website

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import personal from '../assets/images/personal_project_picture.jpeg'
 import group from '../assets/images/group_project.png'
+import enso from '../assets/images/enso.png';
+import paysafe from '../assets/images/paysafe-logo.png';
 
 import '../assets/scss/projectStyle.scss'
 
@@ -56,6 +58,43 @@ class Contact extends React.Component {
                 <div className='image-wrapper'>
                  <img src={group} alt="Group Project" />
              </div>
+            </div>
+
+            <div className='project-card-wrapper'>
+            <div className='project-card-description'>
+                <span className='project-card-title'>Enso Digital</span><br />
+                    
+                    <span>I had been communicating with the owners and designers at enso for months
+                        because I was so impressed with their design and UX, so when they approached
+                        me with a contratc to re-create their website I jumped on board as quickly as possible.
+                        It was my second remote contrat, and the first I took on while working balancing a
+                        full-time job as well.</span><br/>
+                        <span> You can visit the site <a href="https://enso.digital"><span>HERE</span></a><br/>
+                        </span>
+                </div>
+                <div className='image-wrapper'>
+                 <img src={enso} alt="Group Project" />
+               </div>
+               
+            </div>
+
+            <div className='project-card-wrapper'>
+            <div className='project-card-description'>
+                <span className='project-card-title'>Paysafe Group</span><br />
+                    
+                    <span>This has been my first full-time role as a developer.
+                        Working in a large office, with many teams working on different
+                        projects in parallel has been a positive fast-paced learning experience.
+                        I have been mainly involved with the frontend maintanence of the NETELLER
+                        digital wallet. And more recenlty have been involved in developing
+                        new applications for both SKRILL and NETELLER digital wallets.</span><br/>
+                        <span> You can visit the site <a href="https://www.paysafe.com/na-en/"><span>HERE</span></a><br/>
+                        </span>
+                </div>
+                <div className='image-wrapper'>
+                 <img src={paysafe} alt="Group Project" />
+               </div>
+               
             </div>
         </section>
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -9,10 +9,10 @@ const Header = (props) => (
         <Link to="/" className="logo"><span style={{marginLeft: '.75em', fontSize: '9em', letterSpacing: '4px', fontWeight: '400'}}>Low.</span></Link>
         <nav className='site-nav'>
             <ul style={{listStyle: 'none', display: 'flex'}}>
-                <li onClick={ () => window.scrollTo(0, 560)} style={{marginRight: '10px', fontWeight: '300px', color: '#EEE5E9'}}>Me.</li>
-                <li onClick={ () => window.scrollTo(0, 990)} style={{marginRight: '10px', fontWeight: '300px', color: '#EEE5E9'}}>Technology.</li>
-                <li onClick={ () => window.scrollTo(0, 1940)} style={{marginRight: '10px', fontWeight: '300px', color: '#EEE5E9'}}>Portfolio.</li>
-                <li onClick={ () => window.scrollTo(0, 3000)} style={{marginRight: '10px', fontWeight: '300px', color: '#EEE5E9'}}>Connect.</li>                                                
+                <li onClick={ () => window.scrollTo(0, 560)} id='nav-link'>Me.</li>
+                <li onClick={ () => window.scrollTo(0, 990)} id='nav-link'>Technology.</li>
+                <li onClick={ () => window.scrollTo(0, 1940)} id='nav-link'>Portfolio.</li>
+                <li onClick={ () => window.scrollTo(0, 3000)} id='nav-link'>Connect.</li>                                                
             </ul>
         </nav>
     </header>
